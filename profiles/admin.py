57ef8a1c9cc3802bf2a09a088f063de0b4ff6475
Damien <db.webapp@gmail.com>
2023-03-04 22:18:30 +0000

    Searching filters with django-filter

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Profile
+from comment.models import Comment
+from posts.models import Post
 
 admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Post)
