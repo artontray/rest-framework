b3850e303853bbe0625c172f76c944c47e8a857d
Damien <db.webapp@gmail.com>
2023-03-05 11:23:13 +0000

    deploy heroku

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -49,14 +49,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
-if 'DEV' not in os.environ:
-    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'rest_framework.renderers.JSONRenderer',
-    ]
-else:
-    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'rest_framework.renderers.JSONRenderer',
-    ]
+
 
 if 'DEV' in os.environ:
      DATABASES = {
